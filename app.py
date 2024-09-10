1b74d21c0ee5f29c341ee7e5ddbe1d5594d19976
karanchhillar <karan.5716403221@ipu.ac.in>
2024-09-10 20:31:03 +0530

    Ui updated

--- app.py
+++ app.py
@@ -12,20 +12,39 @@ def recommend(movie):
 
     return recommended_movie_names
 
-
-st.header('Movie Recommender System')
 movies = pickle.load(open('movie_dict.pkl','rb'))
 similarity = pickle.load(open('similarity.pkl','rb'))
 
 movies = pd.DataFrame(movies)
 movie_list = movies['title'].values
 
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
+# Improved UI
+st.set_page_config(page_title="Movie Recommender", layout="wide")
+st.title('🎬 Movie Recommender System')
 
+# Sidebar for better organization
+with st.sidebar:
+    st.header("Select Your Movie")
+    selected_movie = st.selectbox(
+        "Type or select a movie from the dropdown",
+        movies['title'].values
+    )
+
+st.subheader("Discover movies similar to your choice!")
+
+# Show recommendations in a column
 if st.button('Show Recommendation'):
-    recommended_movie_names = recommend(selected_movie)
-    for i in recommended_movie_names:
-        st.text(i)
+    with st.spinner("Fetching recommendations..."):
+        recommended_movie_names = recommend(selected_movie)
+
+    st.write("### Top 5 Recommended Movies:")
+
+    # Display recommendations in a single column
+    for movie_name in recommended_movie_names:
+        st.write(f"**{movie_name}**")
+
+# Footer
+st.markdown("""
+    <hr style="border:2px solid #f1f1f1">
+    <center>Created by Karan Chhillar</center>
+""", unsafe_allow_html=True)
