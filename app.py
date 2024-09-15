532255400c47edeff4ab2de0ac7b2b42257958c4
karanchhillar <karan.5716403221@ipu.ac.in>
2024-09-16 00:15:20 +0530

    added poster of the movies

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
 import pickle
 import streamlit as st
 import pandas as pd
+import requests
+
+def poster(movie_name):
+    api_key = "f9160d595192c3f901ccde3d14f796c1"
+    url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={movie_name}"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check for request errors
+        data = response.json()
+
+        if "results" in data and len(data["results"]) > 0:
+            movie_poster = data["results"][0]["poster_path"]
+            return f"https://image.tmdb.org/t/p/w500{movie_poster}"
+        else:
+            print(f"No results found for movie: {movie_name}")
+            return ""
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error while fetching movie data: {e}")
+        return ""
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     
     recommended_movie_names = []
+    recommended_movie_posters = []
     for i in distances[1:6]:
-        recommended_movie_names.append(movies.iloc[i[0]].title)
+        movie_name = movies.iloc[i[0]].title
+        poster_path = poster(movie_name)
+        recommended_movie_names.append(movie_name)
+        recommended_movie_posters.append(poster_path)
 
-    return recommended_movie_names
+    return recommended_movie_names, recommended_movie_posters
 
 movies = pickle.load(open('movie_dict.pkl','rb'))
 similarity = pickle.load(open('similarity.pkl','rb'))
@@ -35,13 +60,35 @@ st.subheader("Discover movies similar to your choice!")
 # Show recommendations in a column
 if st.button('Show Recommendation'):
     with st.spinner("Fetching recommendations..."):
-        recommended_movie_names = recommend(selected_movie)
+        names, posters = recommend(selected_movie)
 
     st.write("### Top 5 Recommended Movies:")
 
-    # Display recommendations in a single column
-    for movie_name in recommended_movie_names:
-        st.write(f"**{movie_name}**")
+    # Set fixed image width for consistency
+    image_width = 180  # Adjust this width according to your needs
+
+    # Adjust spacing between columns
+    col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1, 1], gap="medium")
+
+    with col1:
+        st.write(names[0], align="center")
+        st.image(posters[0], width=image_width)
+
+    with col2:
+        st.write(names[1], align="center")
+        st.image(posters[1], width=image_width)
+
+    with col3:
+        st.write(names[2], align="center")
+        st.image(posters[2], width=image_width)
+
+    with col4:
+        st.write(names[3], align="center")
+        st.image(posters[3], width=image_width)
+
+    with col5:
+        st.write(names[4], align="center")
+        st.image(posters[4], width=image_width)
 
 # Footer
 st.markdown("""
